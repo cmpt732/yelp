d7dbcccd51664bcd838065da01bdd81cabecd114
cmpt732 <qza72@sfu.ca>
2018-11-17 22:27:18 -0800

    Second Commit, initial spring setup.

---
+++ recommendation/src/main/java/com/cmpt732/yelp/recommendation/RecommendationApplication.java
@@ -0,0 +1,12 @@
+package com.cmpt732.yelp.recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendationApplication.class, args);
+	}
+}
